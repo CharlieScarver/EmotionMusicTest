3e669a20c8b1ad0838858e375e1fed8aefd71333
CharlieScarver <s.apostolovd@gmail.com>
2020-03-09 03:52:50 +0200

    Made a CustomAudioTrack class to make use of GetNextFrames
    Separated two music test cases and put a parameter CodeVariant to switch between them

---
+++ MusicTest/CustomAudioTrack.cs
@@ -0,0 +1,41 @@
+﻿using Emotion.Audio;
+using Emotion.IO;
+using Emotion.Common;
+using Emotion.Standard.Audio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicTest
+{
+    class CustomAudioTrack : AudioTrack
+    {
+        public float PlaybackPoint { get; set; }
+
+        public Action Callback { get; set; }
+
+        private bool CallbackInvoked { get; set; }
+
+        public CustomAudioTrack(AudioAsset file) : base(file)
+        {
+        }
+
+        public CustomAudioTrack(AudioAsset file, float playbackPoint, Action callback) : base(file)
+        {
+            this.PlaybackPoint = playbackPoint;
+            this.Callback = callback;
+            this.CallbackInvoked = false;
+        }
+
+        public override int GetNextFrames(int frameCount, Span<byte> buffer)
+        {
+            if (!this.CallbackInvoked && this.PlaybackPoint != 0 && Math.Round(this.Playback, 2) >= this.PlaybackPoint)
+            {
+                this.Callback.Invoke();
+                this.CallbackInvoked = true;
+            }
+
+            return base.GetNextFrames(frameCount, buffer);
+        }
+    }
+}
--- MusicTest/MainScene.cs
+++ MusicTest/MainScene.cs
@@ -5,6 +5,7 @@ using Emotion.IO;
 using Emotion.Scenography;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,25 @@ namespace MusicTest
 {
     class MainScene : IScene
     {
-        public AudioLayer introLayer { get; set; }
-        public AudioLayer mainLayer { get; set; }
-        public AudioLayer secondaryLayer { get; set; }
+        public byte CodeVariant { get; set; }
+        public AudioLayer IntroLayer { get; set; }
+        public AudioLayer MainLayer { get; set; }
+        public AudioLayer SecondaryLayer { get; set; }
 
-        public AudioTrack BackgroundMusic { get; set; }
-        public AudioTrack BackgroundMusicIntro { get; set; }
+        public AudioAsset BackgroundMusic { get; set; }
+        public AudioAsset BackgroundMusicIntro { get; set; }
+
+        public void PlayMainTrackOnMainLayer()
+        {
+            CustomAudioTrack fcMain = new CustomAudioTrack(BackgroundMusic, 90.35f, PlayMainTrackLoopOnIntroLayer);
+            MainLayer.AddToQueue(fcMain);
+        }
+
+        public void PlayMainTrackLoopOnIntroLayer()
+        {
+            CustomAudioTrack fcMain = new CustomAudioTrack(BackgroundMusic, 90.35f, PlayMainTrackOnMainLayer);
+            IntroLayer.AddToQueue(fcMain);
+        }
 
         public void Draw(RenderComposer composer)
         {
@@ -27,55 +41,43 @@ namespace MusicTest
 
         public void Load()
         {
-            BackgroundMusicIntro = new AudioTrack(Engine.AssetLoader.Get<AudioAsset>("fc-intro2.wav"));
-            BackgroundMusic = new AudioTrack(Engine.AssetLoader.Get<AudioAsset>("fc2.wav"));
+            CodeVariant = 2;
 
+            if (CodeVariant == 1)
+            {
+                BackgroundMusicIntro = Engine.AssetLoader.Get<AudioAsset>("Frozen Cave Intro.wav");
+                BackgroundMusic = Engine.AssetLoader.Get<AudioAsset>("Frozen Cave 12.wav");
 
-            secondaryLayer = Engine.Host.Audio.CreateLayer("Secondary layer", 1);
-            secondaryLayer.PlayNext(BackgroundMusicIntro);
-            secondaryLayer.PlayNext(BackgroundMusic);
-            secondaryLayer.PlayNext(BackgroundMusic);
+                IntroLayer = Engine.Host.Audio.CreateLayer("Intro layer", 1);
+                CustomAudioTrack fcIntro = new CustomAudioTrack(BackgroundMusicIntro, 11.29f, PlayMainTrackOnMainLayer);
+                IntroLayer.AddToQueue(fcIntro);
 
-            introLayer = Engine.Host.Audio.CreateLayer("Intro layer", 1);
-            //introLayer.PlayNext(BackgroundMusicIntro);
-            //introLayer.PlayNext(BackgroundMusic);
-
-            mainLayer = Engine.Host.Audio.CreateLayer("Main layer", 1);
+                MainLayer = Engine.Host.Audio.CreateLayer("Main layer", 1);
+            }
+            else if (CodeVariant == 2)
+            {
+                BackgroundMusicIntro = Engine.AssetLoader.Get<AudioAsset>("Frozen Cave Intro 2.wav");
+                BackgroundMusic = Engine.AssetLoader.Get<AudioAsset>("Frozen Cave 12.wav");
+                
+                SecondaryLayer = Engine.Host.Audio.CreateLayer("Secondary layer", 1);
+                SecondaryLayer.AddToQueue(BackgroundMusicIntro);
+                SecondaryLayer.AddToQueue(BackgroundMusic);
+                SecondaryLayer.AddToQueue(BackgroundMusic);
+            }
         }
 
         public void Unload()
         {
-            //Engine.AssetLoader.Destroy("fc-intro.wav");
-            //Engine.AssetLoader.Destroy("fc.wav");
         }
 
         public void Update()
         {
-            //if (Math.Round(BackgroundMusicIntro.Playback, 2) >= 11.29 && Math.Round(BackgroundMusicIntro.Playback, 2) <= 11.3)
-            //{
-            //    mainLayer.PlayNext(BackgroundMusic);
-            //}
-            //else if (Math.Round(BackgroundMusic.Playback, 2) >= 90.35 && Math.Round(BackgroundMusic.Playback, 2) <= 90.36 && introLayer.Status == PlaybackStatus.NotPlaying)
-            //{
-            //    introLayer.PlayNext(BackgroundMusic);
-            //}
-            //else if (Math.Round(BackgroundMusic.Playback, 2) >= 90.35 && Math.Round(BackgroundMusic.Playback, 2) < 90.36 && mainLayer.Status == PlaybackStatus.NotPlaying)
-            //{
-            //    mainLayer.PlayNext(BackgroundMusic);
-            //}
-
-            //if (introLayer.CurrentTrack.File.Name == "fc.wav")
-            //{
-            //    introLayer.LoopingCurrent = true;
-            //}
-
-            if (Math.Round(BackgroundMusic.Playback, 2) >= 0.0 && Math.Round(BackgroundMusicIntro.Playback, 2) <= 0.3)
+            if (CodeVariant == 2)
             {
-                secondaryLayer.Volume = 0.2f;
-            }
-            else if (Math.Round(BackgroundMusic.Playback, 2) >= 1.0 && Math.Round(BackgroundMusicIntro.Playback, 2) <= 1.1)
-            {
-                secondaryLayer.Volume = 1;
+                if (SecondaryLayer.CurrentTrack.File.Name == "Frozen Cave 12.wav")
+                {
+                    SecondaryLayer.LoopingCurrent = true;
+                }
             }
         }
     }
