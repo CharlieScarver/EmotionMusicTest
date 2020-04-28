4e9ea5f86f4906d2684010d86223744536601b38
CharlieScarver <s.apostolovd@gmail.com>
2020-04-28 05:38:18 +0300

    Added DisplaySize, TextureArea and FlipX to Decoration
    Experimenting with a large sprite sheet with a lot of objects
    Trying to create diversity with a small amount of objects

--- MusicTest/GameObjects/Decoration.cs
+++ MusicTest/GameObjects/Decoration.cs
@@ -3,6 +3,7 @@ using Emotion.Graphics;
 using Emotion.Graphics.Objects;
 using Emotion.IO;
 using Emotion.Primitives;
+using Newtonsoft.Json;
 using System.Numerics;
 
 namespace MusicTest.GameObjects
@@ -15,17 +16,27 @@ namespace MusicTest.GameObjects
 
         public TextureAsset TextureAsset { get; set; }
 
+        public Vector2 DisplaySize { get; set; }
+
+        [JsonProperty(Required = Required.AllowNull)]
+        public Rectangle? TextureArea { get; set; }
+
+        public bool FlipX { get; set; }
+
         public ShaderAsset ShadowShader { get; set; }
 
         public int VelocityOffsetX { get; set; }
 
-        public Decoration(string name, string textureName, Vector2 size, Vector3 position) {
+        public Decoration(string name, string textureName, Vector2 size, Vector3 position, Vector2? displaySize = null, Rectangle? textureArea = null, bool flipX = false) {
             Name = name;
             TextureName = textureName;
             Size = size;
             Position = position;
 
             TextureAsset = Engine.AssetLoader.Get<TextureAsset>($"Textures/{TextureName}");
+            DisplaySize = displaySize ?? size;
+            TextureArea = textureArea;
+            FlipX = flipX;
 
             if (Name.Contains("Ceiling Icicle"))
             {
@@ -46,16 +57,18 @@ namespace MusicTest.GameObjects
                 return;
             }
 
-            if (Name.Contains("Icicle"))
+            if (Name.Contains("Icicle") || Name.Contains("Snowy Rock 1 FG"))
             {
                 //TextureAsset.Texture.Tile = false;
                 composer.SetShader(Engine.AssetLoader.Get<ShaderAsset>("Shaders/Blur.xml").Shader);
                 composer.SetShader(ShadowShader.Shader);
                 composer.RenderSprite(
                     Position,
-                    Size,
+                    DisplaySize,
                     new Color(180, 180, 180),
-                    TextureAsset.Texture
+                    TextureAsset.Texture,
+                    TextureArea,
+                    FlipX
                 );
                 //composer.SetShader(null);
                 composer.SetShader(null);
@@ -67,17 +80,32 @@ namespace MusicTest.GameObjects
                     Position,
                     Size,
                     Color.White,
-                    TextureAsset.Texture
+                    TextureAsset.Texture,
+                    TextureArea,
+                    FlipX
                 );
                 //composer.SetShader(null)
+            }
+            else if (Name.Contains("Snow Floor"))
+            {
+                composer.RenderSprite(
+                    Position,
+                    DisplaySize,
+                    Color.White,
+                    TextureAsset.Texture,
+                    TextureArea,
+                    FlipX
+                );
             }
             else
             {
                 composer.RenderSprite(
                     Position,
-                    Size,
+                    DisplaySize,
                     Color.White,
-                    TextureAsset.Texture
+                    TextureAsset.Texture,
+                    TextureArea,
+                    FlipX
                 );
             }
         }
